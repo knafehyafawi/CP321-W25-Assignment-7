9b407572a4238a63b642ed0ef9b246ab63cf6a8a
Knafeh <adnanawad315@gmail.com>
2025-03-29 22:51:44 -0400

    added gunicorn to requirements.txt and imported it just in case to the main py file. bismillah

--- ADNAN_AWAD.py
+++ ADNAN_AWAD.py
@@ -12,6 +12,7 @@ import dash
 from dash import dcc, html
 from dash.dependencies import Input, Output
 import plotly.express as px
+import gunicorn
 
 # Step 1: Create the dataset for FIFA World Cup finals
 data = {
